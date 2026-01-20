31b6593a8c7904726ee0ed18316429f63d8c05c9
اسمك على GitHub <     GitHub>
2026-01-20 16:27:02 +0100

    new feature

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,13 +1,28 @@
-import "./page.css"
+import "./page.css";
+
 export default function About() {
     return (
         <div id="about-page">
             <div id="description">
                 <h2>About us</h2>
                 <p>
-                    We provide high-quality web development services. <br></br>Our goal is to help businesses grow online with modern and responsive websites.
+                    We provide high-quality web development services. <br />
+                    Our goal is to help businesses grow online with modern and responsive websites.
                 </p>
                 <button>Explore Opportunities</button>
+            </div>
+
+            <div className="main-content">
+
+
+                {/* Concept Two */}
+                <div className="concept concept-two">
+                    {["W", "E", "L", "C", "O", "M","E"].map((letter, index) => (
+                        <div key={index} className="hover">
+                            <h1>{letter}</h1>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
