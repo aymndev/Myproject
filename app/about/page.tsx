7adeb5683c71c90ac4ad69cbe864d65f35b4c586
اسمك على GitHub <     GitHub>
2026-01-20 15:41:00 +0100

    update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,14 @@
+import "./page.css"
 export default function About() {
     return (
         <div id="about-page">
-            <h2>About</h2>
-
+            <div id="description">
+                <h2>About us</h2>
+                <p>
+                    We provide high-quality web development services. <br></br>Our goal is to help businesses grow online with modern and responsive websites.
+                </p>
+                <button>Explore Opportunities</button>
+            </div>
         </div>
     );
 }
